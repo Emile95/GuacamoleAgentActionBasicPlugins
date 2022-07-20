0975bfa68c726ead3e9d3590ae427fc40539d355
Émile Desjardins <42349274+Emile95@users.noreply.github.com>
2022-07-20 19:18:03 -0400

    Add project files.

---
+++ Jihogo/JihoGoAction.cs
@@ -0,0 +1,20 @@
+﻿using Library.Agent.Action;
+
+namespace Jihogo
+{
+    public class JihoGoAction : AgentAction
+    {
+        public override AgentActionDefinition GetAgentActionDefinition()
+        {
+            return new AgentActionDefinition
+            {
+                DisplayName = "JihoGo !!"
+            };
+        }
+
+        public override void Process()
+        {
+            Console.WriteLine("Jihooooogoooo");
+        }
+    }
+}
